a8554352062e726e92ccb3cb250c1f3c9e358836
Yaroslav Halchenko <debian@onerussian.com>
2024-10-30 19:46:37 -0400

    Adjust script for populating intendedfor to use copies under code and do custom one as well

--- code/heuristic-intendedfor-imagingvolume.py
+++ code/heuristic-intendedfor-imagingvolume.py
@@ -1,1 +1,4 @@
-../.git/annex/objects/Gx/9Z/MD5E-s108--d7306b311f52e81305976b8cc1814ad6.py/MD5E-s108--d7306b311f52e81305976b8cc1814ad6.py
+POPULATE_INTENDED_FOR_OPTS = {
+    'matching_parameters': ['ImagingVolume', ],
+    'criterion': 'Closest'
+}
